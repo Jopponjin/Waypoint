1c85565295ac620ebf530b998f0cb6213f611ac0
Johannes <johannes404@protonmail.com>
2020-12-26 14:11:34 +0100

    Added assets

---
+++ Assets/Scripts/Walker.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Walker : MonoBehaviour
+{
+    [SerializeField]
+    WaypointSystem waypointSystem;
+
+    public bool shouldPatroll = true;
+
+    bool patrollOnce = false;
+    bool agentMoving = false;
+
+    NavMeshAgent agent;
+    [Space]
+    [SerializeField]
+    float agentSpeed = 0f;
+
+    [SerializeField]
+    float patroleDelay = 0f;
+    [Space]
+
+    [Header("Debug")]
+    [SerializeField]
+    Transform currentTargetPos;
+
+    [SerializeField]
+    int indexer = 0;
+
+    [SerializeField]
+    float tTime = 0;
+
+
+    private void Start()
+    {
+        waypointSystem = GameObject.FindGameObjectWithTag("WaypointManger").GetComponent<WaypointSystem>();
+
+        agent = GetComponent<NavMeshAgent>();
+        agent.SetDestination(waypointSystem.wayPoints[0].transform.position);
+
+        indexer = 0;
+        agent.speed = agentSpeed;
+    }
+
+    private void FixedUpdate()
+    {
+        tTime += Time.deltaTime;
+
+        if (agent.destination != null)
+        {
+            if (!agentMoving & tTime >= patroleDelay & !patrollOnce)
+            {
+                if (indexer <= waypointSystem.wayPoints.Length - 1)
+                {
+                    Debug.Log(currentTargetPos.ToString());
+                    Debug.Log("Agent is moveing to target");
+                    agent.SetDestination(waypointSystem.wayPoints[indexer].transform.position);
+                    agentMoving = true;
+                    currentTargetPos = waypointSystem.wayPoints[indexer].transform;
+                }
+                else
+                {
+                    indexer = 0;
+                    agentMoving = false;
+                    if (!shouldPatroll)
+                    {
+                        patrollOnce = true;
+                    }
+                }
+            }
+            else if (agent.remainingDistance < 2f & agentMoving == true)
+            {
+                Debug.Log("agent stoped");
+                indexer++;
+                tTime = 0;
+                agentMoving = false;
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/WaypointSystem.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class WaypointSystem : MonoBehaviour
+{
+    [Header("Waypoints")]
+    public GameObject[] wayPoints;
+    [Space]
+    float tTime = 0;
+    
+    [Header("Debug")]
+    [SerializeField]
+    bool drawnDebugGizmos = true;
+
+    private void OnDrawGizmos()
+    {
+        if (drawnDebugGizmos)
+        {
+            tTime = Time.deltaTime;
+            if (wayPoints != null)
+            {
+                for (int i = 0; i < wayPoints.Length - 1; i++)
+                {
+                    Gizmos.color = Color.red;
+                    Gizmos.DrawLine(wayPoints[i].transform.position, wayPoints[i + 1].transform.position);
+                    Gizmos.color = Color.cyan;
+                    Gizmos.DrawSphere(wayPoints[i].transform.position, 1f);
+                    Gizmos.color = Color.red;
+                    Gizmos.DrawLine(wayPoints[wayPoints.Length - 1].transform.position, wayPoints[0].transform.position);
+
+                }
+            }
+        }
+    }
+}
